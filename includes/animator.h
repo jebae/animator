16758d9dbade5712c88707a45d94c33c9a379786
Jeongmo BAE <jebae@e1r1p4.42.fr>
2019-07-24 19:38:19 +0200

    add test

--- includes/animator.h
+++ includes/animator.h
@@ -5,34 +5,20 @@
 # include "gmath.h"
 # include "rt.h"
 # include "rt_struct.h"
+# include "rt_parallel.h"
 # include "rt_args.h"
 
 /*
 ** object
 */
-void		sphere_translate(
-	t_mat4 *mat,
-	char *objects_buf
-);
+void		sphere_translate(t_mat4 *mat, char *objects_buf);
 
-void		cylinder_translate(
-	t_mat4 *mat,
-	char *objects_buf
-);
+void		cylinder_translate(t_mat4 *mat, char *objects_buf);
 
-void		cone_translate(
-	t_mat4 *mat,
-	char *objects_buf
-);
+void		cone_translate(t_mat4 *mat, char *objects_buf);
 
-void		plane_translate(
-	t_mat4 *mat,
-	char *objects_buf
-);
+void		plane_translate(t_mat4 *mat, char *objects_buf);
 
-void		triangle_translate(
-	t_mat4 *mat,
-	char *objects_buf
-);
+void		triangle_translate(t_mat4 *mat, char *objects_buf);
 
 #endif
---
+++ includes/animator_test.h
@@ -0,0 +1,9 @@
+#ifndef ANIMATOR_TEST_H
+# define ANIMATOR_TEST_H
+
+#include "animator.h"
+#include "rt_test.h"
+
+void				test_sphere_translation(int parallel_mode);
+
+#endif
