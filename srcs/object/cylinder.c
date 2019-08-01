c5485ece264c551a452486da4abc2c4430b8b79a
Jeongmo BAE <jebae@e1r2p12.42.fr>
2019-08-01 17:13:05 +0200

    add 42 header

--- srcs/object/cylinder.c
+++ srcs/object/cylinder.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cylinder.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jebae <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/01 17:11:06 by jebae             #+#    #+#             */
+/*   Updated: 2019/08/01 17:12:39 by jebae            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "animator.h"
 
 void		cylinder_translate(char *objects_buf, t_mat4 *mat)
@@ -10,7 +22,7 @@ void		cylinder_translate(char *objects_buf, t_mat4 *mat)
 
 void		cylinder_rotate(char *objects_buf, t_vec4 *n, float theta)
 {
-	t_cylinder			*cylinder;
+	t_cylinder		*cylinder;
 	t_quaternion	q;
 	t_quaternion	q_i;
 
